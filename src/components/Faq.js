00eb5dfd420653f949aab6ffdfbe3f1fcd16dcd6
jacquelineb <jacquelinesotheremail@gmail.com>
2021-07-22 21:42:21 -0700

    Created FAQ

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Faq() {
-  return <div>FAQ</div>;
+  const [faq, setFaq] = useState([]);
+
+  useEffect(() => {
+    fetch('data/faq.json')
+      .then((res) => res.json())
+      .then((data) => setFaq(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>FAQ</h1>
+      {faq.map((faqItem) => {
+        return (
+          <div key={faqItem.id}>
+            <p>{faqItem.question}</p>
+            <p>{faqItem.answer}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Faq;
