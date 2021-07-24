6de35ab46a764641912119ea611125e11c2bb28a
jacquelineb <jacquelinesotheremail@gmail.com>
2021-07-23 23:33:37 -0700

    Added media queries

--- src/components/About.js
+++ src/components/About.js
@@ -3,18 +3,17 @@ import './About.css';
 
 function About() {
   return (
-    <>
+    <section className='about'>
       <h2>About</h2>
-      <p className='about'>
+      <p className='dev-description'>
         Camelot Software Planning is a Japanese video game developer founded in 1990 as a studio
         within SEGA. In 2001, Camelot went on to form a partnership with Nintendo. They are
         known for for creating games such as <span className='title'>Mario Tennis</span>,{' '}
         <span className='title'>Mario Golf</span>, and the{' '}
         <span className='title'>Shining</span> series.
       </p>
-
       <h2>Dev Blog</h2>
-    </>
+    </section>
   );
 }
 
