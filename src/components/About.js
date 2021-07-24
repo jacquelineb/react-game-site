8bd232340dc9ae8a884f38c1eea71910b86e96ef
jacquelineb <jacquelinesotheremail@gmail.com>
2021-07-23 19:06:30 -0700

    Added content to About / Developer page

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import './About.css';
 
 function About() {
-  return <div>About</div>;
+  return (
+    <>
+      <h2>About</h2>
+      <p className='about'>
+        Camelot Software Planning is a Japanese video game developer founded in 1990 as a studio
+        within SEGA. In 2001, Camelot went on to form a partnership with Nintendo. They are
+        known for for creating games such as <span className='title'>Mario Tennis</span>,{' '}
+        <span className='title'>Mario Golf</span>, and the{' '}
+        <span className='title'>Shining</span> series.
+      </p>
+
+      <h2>Dev Blog</h2>
+    </>
+  );
 }
 
 export default About;
